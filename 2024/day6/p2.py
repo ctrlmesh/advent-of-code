e1ff68f99cda86ee15fb36eef262a187458d3002
CtrlMesh <190288961+ctrlmesh@users.noreply.github.com>
2024-12-08 01:55:35 +0100

    finally working part 2 answer

--- 2024/day6/p2.py
+++ 2024/day6/p2.py
@@ -1,28 +1,24 @@
 INPUT_FILE = './inputs/input.txt'
 
-def travel_grid(grid, guard_pos, direction = (-1, 0)):
-    visited = set()
+def travel_grid(grid, guard_pos, direction=(-1, 0)):
     g_y, g_x = guard_pos
     d_y, d_x = direction
+    visited = set()
+
     while True:
         next_y, next_x = g_y + d_y, g_x + d_x
         if next_y not in range(0, len(grid)) or next_x not in range(0, len(grid[0])):
             break
-        
-        if (g_y, g_x, d_y, d_x) in visited:
-            print('LOOP DETECTED')
+
+        if (next_y, next_x, d_y, d_x) in visited:
             return -1
-        visited.add((g_y, g_x, d_y, d_x))
-        # grid[g_y][g_x] = 'X'
-     
+        visited.add((next_y, next_x, d_y, d_x))
+
         if grid[next_y][next_x] == '#':
-            # Rotate direction vector 90 degrees the easy way
+            # Rotate direction vector by 90 degrees the easy way
             d_y, d_x = (0, -d_y) if d_y != 0 else (d_x, 0)
         else:
             g_y, g_x = next_y, next_x
-        
-    print('FINISHED')
-    print(visited)
     return visited
 
 with open(INPUT_FILE, 'r') as f:
@@ -33,22 +29,22 @@ with open(INPUT_FILE, 'r') as f:
         if '^' in row:
             g_y, g_x = col_index, row.index('^')
 
+start_pos = (g_y, g_x)
+
 loops = 0
-for state in travel_grid(grid, (g_y, g_x)):
-    g_y, g_x, d_y, d_x = state
-    prev = grid[g_y + d_y][g_x + d_x]
-    grid[g_y + d_y][g_x + d_x] = '#'
-    if travel_grid(grid, (g_y, g_x), (d_y, d_x)) == -1:
-        loops += 1
-    grid[g_y + d_y][g_x + d_x] = prev
+placed = set()
+
+initial_path = travel_grid(grid, (g_y, g_x))
+if initial_path != -1:
+    for pos_y, pos_x, d_y, d_x in initial_path:
+        if (pos_y, pos_x) in placed or (pos_y, pos_x) == start_pos:
+            continue
+
+        prev = grid[pos_y][pos_x]
+        grid[pos_y][pos_x] = '#'
+        if travel_grid(grid, start_pos) == -1:
+            placed.add((pos_y, pos_x))
+            loops += 1
+        grid[pos_y][pos_x] = prev
 
 print(loops)
-        
-
-# travel_grid(grid, (g_y, g_x))
-# grid_map = '\n'.join([''.join(row) for row in grid])
-
-# print(grid_map)
-# print(grid_map.count('X'))
-        
-        
